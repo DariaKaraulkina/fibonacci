2935acaba8b01bd3e13164d730748ce55d2d68b3
DariaKaraulkina <D.morgunovaa@mail.ru>
2024-02-06 16:39:15 +0300

    выполнено 1е задание

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,41 @@
+/*Домашнее задание
+1.У нас есть метод, который выводит в консоль последовательность Фибоначчи, ограничивая её числом, значение которого
+мы подаём на вход метода.
+Нужно переписать существующий метод (либо написать новый на базе прежнего), который будет делать то же самое,
+но только ограничивать выводимую последовательность нужно не максимальным значением числа, а количеством чисел в
+последовательности. Т.е. на вход нужно подавать целое число, которое будет говорить СКОЛЬКО всего чисел последовательности
+Фибоначчи нужно вывести в консоль.
+2.На базе последней реализации метода создать новый или исправить существующий метод, также принимающий на вход кол-во
+элементов последовательности, которую нам нужно вывести в консоль. Но новый метод должен возвращать эту самую
+последовательность Фибоначчи в виде массива целых чисел. Т.е. задание остаётся аналогичным заданию в п1 за той разницей,
+что метод теперь должен возвращать массив целых чисел. */
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        fibonacci(0);
+        fibonacci1pro(6);
     }
 
-    public static void fibonacci(int maxNumber) {
+    public static void fibonacci0(int maxNumber) { //исходный метод
         int n0 = 1;
         int n1 = 1;
         int n2;
         while (n0 + n1 <= maxNumber) {
+            System.out.println(n0 + " " + n1 + " ");
+            n2 = n0 + n1;
+            System.out.println(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+        System.out.println();
+    }
+    public static void fibonacci1(int maxNumber) { //более наглядная реализация
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        int[] myArray = new int[maxNumber];
+        int i;
+        for (i = 0; i < myArray.length; i++) {
+            myArray[i] = i + 1;
             System.out.print(n0 + " " + n1 + " ");
             n2 = n0 + n1;
             System.out.print(n2 + " ");
@@ -16,11 +44,20 @@ public class Main {
         }
         System.out.println();
     }
-
-    public static void kassaEpta(String productEpta) {
-        System.out.println("Берем в руки " + productEpta);
-        System.out.println("Сканируем штрикод");
-        System.out.println("Отображается цена в аппарате");
-        System.out.println(productEpta + "пробили, ура!");
+    public static void fibonacci1pro(int maxNumber) { //финальная реализация
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        int[] myArray = new int[maxNumber];
+        int i;
+        for (i = 0; i < myArray.length; i++) {
+            myArray[i] = i + 1;
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+        System.out.println();
     }
 }
+
